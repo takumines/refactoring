126accaa1f004c014a35847032febf6ee4096072
takumines <takumines1@gmail.com>
2024-07-03 00:19:39 +0900

    ✏️

---
+++ 6章/パラメータオブジェクトの導入/example.js
@@ -0,0 +1,60 @@
+/*
+ |--------------------------------------------------------------------------
+ | リファクタリング前
+ |--------------------------------------------------------------------------
+ */
+const station = {
+  name: 'Zushi',
+  readings: [
+    {temp: 47, time: '2016-11-10 09:10' },
+    {temp: 53, time: '2016-11-10 09:20' },
+    {temp: 58, time: '2016-11-10 09:30' },
+    {temp: 53, time: '2016-11-10 09:40' },
+    {temp: 51, time: '2016-11-10 09:50' }
+  ]
+}
+
+function readingsOutsideRange(station, min, max) {
+  return station.readings.filter(r => r.temp < min || r.temp > max)
+}
+
+// 呼び出し側のコード
+alerts = readingsOutsideRange(
+  station,
+  operationgPlan.temperatureFloor,
+  operationgPlan.temperatureCeiling
+)
+
+/*
+ |--------------------------------------------------------------------------
+ | リファクタリング後
+ |--------------------------------------------------------------------------
+ */
+// 値オブジェクトを作成し、振る舞いをもたせる
+class NumberRange {
+  constructor(min, max) {
+    this._data = {min: min, max: max}
+  }
+
+  get min() {
+    return this._data.min
+  }
+
+  get max() {
+    return this._data.max
+  }
+
+  contains(arg) {
+    return arg >= this.min && arg <= this.max
+  }
+}
+
+const range = new NumberRange(
+  operationgPlan.temperatureFloor,
+  operationgPlan.temperatureCeiling
+)
+
+// 呼び出し側のコードの引数に値オブジェクトのインスタンを渡し、振る舞いを呼び出す
+function readingsOutsideRange(station, range) {
+  return station.readings.filter(r => !range.contains(r.temp))
+}
---
+++ 6章/関数群のクラスへの集約/example.js
@@ -0,0 +1,57 @@
+/*
+ |--------------------------------------------------------------------------
+ | リファクタリング前
+ |--------------------------------------------------------------------------
+ */
+// 下記の毎月紅茶メータのデータに対して計算を行うコードが散漫している前提
+reading = {customer: "ivan", quantity: 10, month: 5, year: 2017};
+
+// 基本料金の計算
+const aReading = acquireReading();
+const baseCharge = baseRate(aReading.month, aReading.year) * aReading.quantity;
+
+// 生活必需品は無税
+const aReading = acquireReading();
+const base = baseRate(aReading.month, aReading.year) * aReading.quantity;
+const taxableCharge = Math.max(0, base - taxThreshold(aReading.year));
+
+// 別の場所では、関数の抽出が行われている
+const aReading = acquireReading();
+const basicChargeAmount = calculateBaseCharge(aReading);
+function calculateBaseCharge(aReading) {
+  return baseRate(aReading.month, aReading.year) * aReading.quantity;
+}
+
+/*
+ |--------------------------------------------------------------------------
+ | リファクタリング後
+ |--------------------------------------------------------------------------
+ */
+// 関数とデータの結びつきを強くするために、レコードのカプセル化を行いクラスを作る
+class Reading {
+  constructor (data) {
+    this._customer = data.customer;
+    this._quantity = data.quantity;
+    this._month = data.month;
+    this._year = data.year;
+  }
+
+  get customer() { return this._customer }
+  get quantity() { return this._quantity }
+  get month() { return this._month }
+  get year() { return this._year }
+
+  // 関数の移動を行い、クラス内に閉じ込めて、命名を変更
+  get baseCharge() {
+    return baseRate(this.month, this.year) * this.quantity;
+  }
+
+  get taxableCharge() {
+    return Math.max(0, this.baseCharge - taxThreshold(this.year));
+  }
+}
+
+// 呼び出し側
+const rawReading = acquireReading();
+const aReading = new Reading(rawReading);
+const taxableCharge = aReading.taxableCharge;
